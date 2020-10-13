c9db499869f8baa96e8c9c1457561005e295d8b2
parthtt <pthakka5@my.centennialcollege.ca>
2020-10-13 03:05:34 -0400

    added second activity

---
+++ app/src/main/java/parth/thakkar/parthlab2/thakkarSecondScreen.java
@@ -0,0 +1,14 @@
+package parth.thakkar.parthlab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class thakkarSecondScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thakkar_second_screen);
+    }
+}
